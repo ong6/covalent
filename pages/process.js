d97e8e8f9afc3be1675e124b45eb5d1d2a3cd0fc
Junx <junxiongong2@gmail.com>
2022-04-03 20:15:48 +0800

    updated process wording

--- pages/process.js
+++ pages/process.js
@@ -24,16 +24,26 @@ export default function Home() {
 						<div className="text-2xl font-medium">Design Process</div>
 						<div className="text-lg mt-2">
 							Before starting the design process, I had to identify who my app
-							was going to be for. I choose my target audience to be creators
-							who focus mainly on video content.
+							was going to be for. Below are the steps I took to gather the user
+							requirements as well as my prototyping stage.
 						</div>
-						<div className="text-lg mt-2">
-							So I designed the following persona to help focus on the
-							experience of that main actor.
-						</div>
+						<div className="text-xl font-medium mt-2">Primary User Group</div>
+						<ol className="text-lg list-disc list-inside py-4">
+							<li className="li">
+								Social media users who are focused on video content
+							</li>
+							<li className="li">
+								Looking for a way to manage all their videos in one place
+							</li>
+							<li className="li">
+								Primarily use a laptop/ desktop for video production and editing
+							</li>
+							<li className="li">Making money from producing videos</li>
+						</ol>
 					</div>
 				</Section>
 				<Section delay={0.2}>
+					<div className="text-xl font-medium">Persona</div>
 					<div className="flex justify-center py-4 text-xl">
 						<Image
 							src={persona}
@@ -45,6 +55,7 @@ export default function Home() {
 				</Section>
 				<Section delay={0.2}>
 					<div className="text-lg mt-6">
+						<div className="text-xl font-medium">User Tasks</div>
 						<div className="text-lg mt-2">
 							Then based on this user group and the persona, I came up with the
 							following user tasks.
@@ -63,6 +74,7 @@ export default function Home() {
 								without having to go to each of them separately.
 							</li>
 						</ol>
+						<div className="text-xl font-medium">Lo-Fi Prototyping</div>
 						<div className="div">
 							From these user tasks, I determined that a web application with a
 							dashboard style would suit this product the best and since most
@@ -70,14 +82,32 @@ export default function Home() {
 							complementary addition to their workflow.
 						</div>
 						<div className="mt-2">
+							I then listed out a few key screens and features that I wanted to
+							include in my prototypes.
+						</div>
+						<ol className="text-lg list-disc list-inside py-4">
+							<li className="li">Dashboard to view all information</li>
+							<li className="li">
+								Teams panel to manage team members - Since nowadays video
+								creators work with a large team of members
+							</li>
+							<li className="li">Nice looking charts</li>
+							<li className="li">
+								Beautiful to look at, and navigable reports
+							</li>
+							<li className="li">List of videos</li>
+							<li className="li">Video analytics and management tools</li>
+						</ol>
+						<div className="mt-2">
 							I then hand drew simple wireframes to represent each user task to
 							get a clearer image of what it was going to look like. After a few
-							iterations, I ended up with the following storyboards/ prototypes:
+							iterations, I ended up with the following storyboards:
 						</div>
 					</div>
 				</Section>
 				<Section delay={0.3}>
-					<div className="flex justify-center py-4 text-xl">
+					<div className="flex flex-col justify-center py-4 text-xl">
+						<div className="text-xl font-medium">Storyboard 1</div>
 						<Image
 							src={wireframe_01}
 							alt="covalent"
@@ -87,7 +117,8 @@ export default function Home() {
 					</div>
 				</Section>
 				<Section delay={0.3}>
-					<div className="flex justify-center py-4 text-xl">
+					<div className="flex flex-col justify-center py-4 text-xl">
+						<div className="text-xl font-medium">Storyboard 2</div>
 						<Image
 							src={wireframe_02}
 							alt="covalent"
@@ -96,10 +127,77 @@ export default function Home() {
 						/>
 					</div>
 				</Section>
-				<Section delay={0.5}>
+				<Section delay={0.4}>
 					<div className="text-lg mt-4">
 						<div className="text-lg mt-2">
-							Some of the design decisions I faced when working on the
+							After designing the Storyboards I devised a user flow that would
+							fit with the design of the app.
+						</div>
+						<div className="text-xl font-medium mt-2">
+							User Flow 1 - Analytics
+						</div>
+						<ol className="text-lg list-decimal list-inside py-4">
+							<li className="li">
+								User logs in with their account and is redirected to the
+								dashboard.
+							</li>
+							<li className="li">
+								User can choose to view the videos they have posted or view the
+								analytics data.
+							</li>
+							<li className="li">
+								User chooses to view the analytics data and see a page
+								containing a few graphs and can choose the video platform they
+								wish to see the data for.
+							</li>
+						</ol>
+						<div className="text-xl font-medium mt-2">User Flow 2 - Report</div>
+						<ol className="text-lg list-decimal list-inside py-4">
+							<li className="li">
+								User logs in with their account and is redirected to the
+								dashboard.
+							</li>
+							<li className="li">
+								User goes to the Report page to view the business data for the
+								day.
+							</li>
+							<li className="li">
+								User is able to clearly and easily pick out information they
+								need.
+							</li>
+							<li className="li">
+								User can toggle between different reports and see the relevant
+								data.
+							</li>
+						</ol>
+						<div className="text-xl font-medium mt-2">User Flow 3 - Videos</div>
+						<ol className="text-lg list-decimal list-inside py-4">
+							<li className="li">
+								User logs in with their account and is redirected to the
+								dashboard.
+							</li>
+							<li className="li">
+								User goes to the Videos page to view the previously uploaded
+								videos and scheduled videos
+							</li>
+							<li className="li">
+								User is able to clearly see the view data, the thumbnail, and
+								the title of the video from the list.
+							</li>
+							<li className="li">
+								User can then view the analytics of each video in the list.
+							</li>
+						</ol>
+						<div className="mt-2">
+							I then moved on to evaluate some of the past designs I created.
+						</div>
+					</div>
+				</Section>
+				<Section delay={0.5}>
+					<div className="text-lg mt-4">
+						<div className="text-xl font-medium">Design Findings</div>
+						<div className="text-lg mt-2">
+							Some of the design decisions I weighed while working on the
 							wireframe:
 						</div>
 						<ol className="text-lg list-disc list-inside py-4">
@@ -107,7 +205,8 @@ export default function Home() {
 								Using a dashboard style vs using a traditional style webpage.
 							</li>
 							<li className="li">
-								What tabs do I want to showcase on the sidebar.
+								The relevant tabs on the sidebar, should the side bar be
+								collapsable
 							</li>
 							<li className="li">
 								Usage of graphs and if they add value to the user especially
@@ -121,7 +220,7 @@ export default function Home() {
 							be able to see the data in a more organized way.
 						</div>
 						<div className="mt-2">
-							Once the Low-Fi mockups where done, I started to design the Hi-Fi
+							Once these steps where done, I started to design the High-Fi
 							version of the web application on Figma.
 						</div>
 					</div>
